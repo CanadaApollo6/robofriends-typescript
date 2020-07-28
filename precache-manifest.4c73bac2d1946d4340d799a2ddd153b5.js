fad192984715f18370508905dede94f7ab0d9dc3
CanadaApollo6 <riel.stamand@gmail.com>
2020-07-28 14:29:58 -0400

    Updates

---
+++ precache-manifest.4c73bac2d1946d4340d799a2ddd153b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9131921feef2d01803af3d9b0c5a3a",
+    "url": "/robofriends-typescript/index.html"
+  },
+  {
+    "revision": "5f81d6c6910cd916628c",
+    "url": "/robofriends-typescript/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d44e0354f0573d6fdb28",
+    "url": "/robofriends-typescript/static/css/main.435565c1.chunk.css"
+  },
+  {
+    "revision": "5f81d6c6910cd916628c",
+    "url": "/robofriends-typescript/static/js/2.c8fac1ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-typescript/static/js/2.c8fac1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44e0354f0573d6fdb28",
+    "url": "/robofriends-typescript/static/js/main.4ab997c2.chunk.js"
+  },
+  {
+    "revision": "dad09c3c5ea84b596ccd",
+    "url": "/robofriends-typescript/static/js/runtime-main.e7f99633.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-typescript/static/media/SEGA.12a3db8b.woff"
+  }
+]);
